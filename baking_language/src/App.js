edac263924bfd86beae84f12722fdcda43fae919
fullmoon-sg <mbfoo7757@gmail.com>
2021-01-28 08:16:44 +0000

    Managed to load data via axios

--- baking_language/src/App.js
+++ baking_language/src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
+import React from 'react'
+import {BrowserRouter as Router,Link,Switch,Route} from 'react-router-dom'
+import axios from 'axios'
+import 'bootstrap/dist/css/bootstrap.min.css'
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component{
+
+    state = { 
+        products : []
+    }
+
+    async componentDidMount(){
+        let response = await axios.get('https://8080-de3f3226-69d2-47aa-8bc2-2faf1e0a30b5.ws-us03.gitpod.io/products-api');
+        this.setState({
+            products : response.data
+        })
+    }
+    render(){
+        return <div className="App"></div>      
+    }
+
 }
 
 export default App;
